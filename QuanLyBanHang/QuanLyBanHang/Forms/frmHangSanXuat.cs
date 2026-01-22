175af01dfe495371c2e3530424b13e22226732eb
Yen Vy <vuthiyenvy000@gmail.com>
2026-01-22 23:31:20 +0700

    th1

---
+++ QuanLyBanHang/QuanLyBanHang/Forms/frmHangSanXuat.cs
@@ -0,0 +1,115 @@
+﻿using QuanLyBanHang.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang.Forms
+{
+    public partial class frmHangSanXuat : Form
+    {
+        public frmHangSanXuat()
+        {
+            InitializeComponent();
+        }
+        QLBHDbContext context = new QLBHDbContext(); // Khởi tạo biến ngữ cảnh CSDL
+        bool xuLyThem = false; // Kiểm tra có nhấn vào nút Thêm hay không?
+        int id; // Lấy mã loại sản phẩm (dùng cho Sửa và Xóa
+
+        private void BatTatChucNang(bool giaTri)
+        {
+            btnLuu.Enabled = giaTri;
+            btnHuyBo.Enabled = giaTri;
+            txtTenHangSanXuat.Enabled = giaTri;
+            btnThem.Enabled = !giaTri;
+            btnSua.Enabled = !giaTri;
+            btnXoa.Enabled = !giaTri;
+        }
+        private void frmHangSanXuat_Load(object sender, EventArgs e)
+        {
+            BatTatChucNang(false);
+            List<HangSanXuat> lsp = new List<HangSanXuat>();
+            lsp = context.HangSanXuat.ToList();
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = lsp;
+            txtTenHangSanXuat.DataBindings.Clear();
+            txtTenHangSanXuat.DataBindings.Add("Text", bindingSource, "TenHangSanXuat", false, DataSourceUpdateMode.Never);
+            dataGridView.DataSource = bindingSource;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            xuLyThem = true;
+            BatTatChucNang(true);
+            txtTenHangSanXuat.Clear();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            xuLyThem = false;
+            BatTatChucNang(true);
+            id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value.ToString());
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận xóa hãng sản xuất?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value.ToString());
+                HangSanXuat hsp = context.HangSanXuat.Find(id);
+                if (hsp != null)
+                {
+                    context.HangSanXuat.Remove(hsp);
+                }
+                context.SaveChanges();
+                frmHangSanXuat_Load(sender, e);
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTenHangSanXuat.Text))
+                MessageBox.Show("Vui lòng nhập tên hãng sản xuất?", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (xuLyThem)
+                {
+                    HangSanXuat hsp = new HangSanXuat();
+                    hsp.TenHangSanXuat = txtTenHangSanXuat.Text;
+                    context.HangSanXuat.Add(hsp);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    HangSanXuat hsp = context.HangSanXuat.Find(id);
+                    if (hsp != null)
+                    {
+                        hsp.TenHangSanXuat = txtTenHangSanXuat.Text;
+                        context.HangSanXuat.Update(hsp);
+                        context.SaveChanges();
+                    }
+                }
+                frmHangSanXuat_Load(sender, e);
+            }
+        }
+
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            frmHangSanXuat_Load(sender, e);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult thoat = MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (thoat == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
